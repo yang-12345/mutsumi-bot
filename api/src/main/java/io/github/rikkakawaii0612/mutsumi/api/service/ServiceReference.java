1815369298850da10d1b5aeac30f80f160b4fd0d
yang_12345 <1716437252@qq.com>
2026-04-25 21:07:16 +0800

    新增: ServiceReference.callAsync 方法

--- api/src/main/java/io/github/rikkakawaii0612/mutsumi/api/service/ServiceReference.java
+++ api/src/main/java/io/github/rikkakawaii0612/mutsumi/api/service/ServiceReference.java
@@ -2,10 +2,17 @@ package io.github.rikkakawaii0612.mutsumi.api.service;
 
 import io.github.rikkakawaii0612.mutsumi.api.service.data.ObjectData;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface ServiceReference {
     Service get();
 
     default ObjectData call(ServiceRequest request) {
         return this.get().call(request);
+    }
+
+    default List<ObjectData> callAsync(Collection<ServiceRequest> requests) {
+        return this.get().callAsync(requests);
     }
 }
