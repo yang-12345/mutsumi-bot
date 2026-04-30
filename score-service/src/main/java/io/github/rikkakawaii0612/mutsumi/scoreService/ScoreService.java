472f7227fee9fe1076959ca64ea2986a3d4617a6
RikkaKawaii0612 <rikkakawaii0612@outlook.com>
2026-04-30 14:38:51 +0800

    更改: ScoreService 翻新

--- score-service/src/main/java/io/github/rikkakawaii0612/mutsumi/scoreService/ScoreService.java
+++ score-service/src/main/java/io/github/rikkakawaii0612/mutsumi/scoreService/ScoreService.java
@@ -1,52 +1,40 @@
 package io.github.rikkakawaii0612.mutsumi.scoreService;
 
+import io.github.rikkakawaii0612.mutsumi.api.Service;
+import io.github.rikkakawaii0612.mutsumi.api.ServiceLookup;
 import io.github.rikkakawaii0612.mutsumi.api.contact.Group;
 import io.github.rikkakawaii0612.mutsumi.api.contact.Member;
 import io.github.rikkakawaii0612.mutsumi.api.contact.MutsumiBot;
 import io.github.rikkakawaii0612.mutsumi.api.contact.message.Message;
-import io.github.rikkakawaii0612.mutsumi.api.handler.MessageHandler;
-import io.github.rikkakawaii0612.mutsumi.api.service.Service;
-import io.github.rikkakawaii0612.mutsumi.api.service.ServiceReference;
-import io.github.rikkakawaii0612.mutsumi.api.service.ServiceRequest;
-import io.github.rikkakawaii0612.mutsumi.api.service.data.ListObjectData;
-import io.github.rikkakawaii0612.mutsumi.api.service.data.ObjectData;
+import io.github.rikkakawaii0612.mutsumi.api.util.MutsumiUtils;
 import io.github.rikkakawaii0612.mutsumi.api.util.Pair;
-import org.pf4j.Extension;
+import io.github.rikkakawaii0612.mutsumi.osuApi.OsuApiService;
+import io.github.rikkakawaii0612.mutsumi.osuApi.data.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
+import java.util.function.Supplier;
 
-@Extension
-public class ScoreService extends Service implements MessageHandler {
+public class ScoreService implements Service {
     private static final Logger LOGGER = LoggerFactory.getLogger("ScoreService");
 
-    private ServiceReference osuApiService;
+    private OsuApiService osuApiService;
 
     public ScoreService() {
     }
 
     @Override
-    public ObjectData call(ServiceRequest request) {
-        return ObjectData.EMPTY;
-    }
-
-    @Override
-    public void load() {
-        this.osuApiService = this.getModule().getServiceLocator().getService("osu-api-base");
+    public void load(String id, ServiceLookup lookup) {
+        this.osuApiService = (OsuApiService) lookup.getService("osu-api").service();
+        lookup.getMutsumi().getBotBus().addMessageHandler(this::onHandleMessage);
     }
 
     @Override
     public void unload() {
     }
 
-    @Override
-    public String getId() {
-        return "guess-service";
-    }
-
-    @Override
-    public void handleMessage(MutsumiBot bot, Group group, Member sender, Message message) {
+    public void onHandleMessage(MutsumiBot bot, Group group, Member sender, Message message) {
         String m = message.asString();
         String str = m.trim();
         if (!str.startsWith("!") && !str.startsWith("！") && !str.startsWith("/")) {
@@ -55,55 +43,55 @@ public class ScoreService extends Service implements MessageHandler {
 
         if (str.substring(1).startsWith("bp ")) {
             String param = str.substring(4);
-            ObjectData /*User*/ user = this.osuApiService.call(ServiceRequest.builder()
-                    .header("service", "getUser")
-                    .header("username", param)
-                    .build());
+            Optional<User> optional = this.osuApiService.getUser(param);
 
-            if (user.isEmpty()) {
+            if (optional.isEmpty()) {
                 bot.sendMessage(group.getId(), Message.at(sender.getId())
                         .append(Message.text(" 没有找到用户名或ID为 " + param + " 的用户。")));
                 return;
             }
 
-            String username = user.getString("name");
-            String id = user.getString("id");
+            User user = optional.get();
+
+            String username = user.username;
+            long id = user.id;
 
             bot.sendMessage(group.getId(), Message.at(sender.getId())
                     .append(Message.text(" 正在查找用户 " + username + " 的bp200信息……" +
                             "\n这可能需要一些时间。")));
 
-            ObjectData /*List<Score>*/ scores = this.osuApiService.call(ServiceRequest.builder()
-                    .header("service", "getBestScores")
-                    .header("id", id)
-                    .header("mode", "mania")
-                    .build());
-            List<? extends ObjectData /*Score*/> list = ListObjectData.readAsMutable(scores);
-            list.sort(Comparator
-                    .comparingDouble((ObjectData /*Score*/ value)
-                            -> value.getDouble("pp"))
+            Optional<List<Score>> optional2 = this.osuApiService.getBestScores(id, PlayMode.MANIA);
+            if (optional2.isEmpty()) {
+                bot.sendMessage(group.getId(), Message.at(sender.getId())
+                        .append(Message.text(" 没有找到用户 " + username + " 的 BP200 成绩。")));
+                return;
+            }
+
+            List<Score> scores = optional2.get();
+            scores.sort(Comparator
+                    .comparingDouble((Score score) -> score.pp)
                     .reversed());
 
             double pp = 0.0D, multiplier = 1.0D;
-            for (ObjectData objectData : list) {
-                pp += multiplier * objectData.getDouble("pp");
+            for (Score score : scores) {
+                pp += multiplier * score.pp;
                 multiplier *= 0.95D;
             }
 
             // 计算 BonusPP 没有意义了
             StringBuilder builder = new StringBuilder(" 用户 " + username
-                    + " 共找到 " + list.size()
+                    + " 共找到 " + scores.size()
                     + " 个有效成绩，总pp（不包括BonusPP）：" + pp);
 
-            int limit = Math.min(10, list.size());
+            int limit = Math.min(10, scores.size());
             for (int i = 0; i < limit; i++) {
-                ObjectData /*Beatmapset*/ beatmapset = list.get(i).get("beatmapset");
+                Beatmapset beatmapset = scores.get(i).beatmapset;
                 builder.append("\n")
-                        .append(beatmapset.getString("artist"))
+                        .append(beatmapset.artist)
                         .append(" - ")
-                        .append(beatmapset.getString("title"))
+                        .append(beatmapset.title)
                         .append(": ")
-                        .append(list.get(i).getDouble("pp"))
+                        .append(scores.get(i).pp)
                         .append("pp");
             }
 
@@ -112,48 +100,49 @@ public class ScoreService extends Service implements MessageHandler {
 
         if (str.substring(1).startsWith("bp7k ")) {
             String param = str.substring(6);
-            ObjectData /*User*/ user = this.osuApiService.call(ServiceRequest.builder()
-                    .header("service", "getUser")
-                    .header("username", param)
-                    .build());
+            Optional<User> optional = this.osuApiService.getUser(param);
 
-            if (user.isEmpty()) {
+            if (optional.isEmpty()) {
                 bot.sendMessage(group.getId(), Message.at(sender.getId())
                         .append(Message.text(" 没有找到用户名或ID为 " + param + " 的用户。")));
                 return;
             }
 
-            String username = user.get("name").asString();
-            String id = user.get("id").asString();
+            User user = optional.get();
+
+            String username = user.username;
+            long id = user.id;
 
             bot.sendMessage(group.getId(), Message.at(sender.getId())
                     .append(Message.text(" 正在查找用户 " + username + " 的7k信息……" +
                             "\n这可能需要一些时间。")));
 
-            List<? extends ObjectData /*BeatmapPlayCount*/>
-                    allPlayedBeatmaps = ListObjectData.readAsMutable(
-                            this.osuApiService.call(ServiceRequest.builder()
-                                    .header("service", "getAllPlayedBeatmaps")
-                                    .header("id", id)
-                                    .build()));
+            Optional<List<BeatmapPlayCount>> optional2 = this.osuApiService.getAllPlayedBeatmaps(id);
+            if (optional2.isEmpty()) {
+                bot.sendMessage(group.getId(), Message.at(sender.getId())
+                        .append(Message.text(" 没有找到用户 " + username + " 的 BP200 成绩。")));
+                return;
+            }
+
+            List<BeatmapPlayCount> allPlayedBeatmaps = optional2.get();
 
             // TODO: REMOVAL LOG
             LOGGER.info("allPlayedBeatmaps size: {}", allPlayedBeatmaps.size());
 
-            List<List<ObjectData /*Beatmap*/>> beatmapsToPost = new ArrayList<>();
-            for (Iterator<? extends ObjectData> iterator = allPlayedBeatmaps.iterator(); iterator.hasNext(); ) {
-                List<ObjectData /*Beatmap*/> beatmapList = new ArrayList<>();
+            List<List<Beatmap>> beatmapsToPost = new ArrayList<>();
+            for (Iterator<BeatmapPlayCount> iterator = allPlayedBeatmaps.iterator(); iterator.hasNext(); ) {
+                List<Beatmap> beatmapList = new ArrayList<>();
 
                 // 收集50个谱面一组进行post
                 while (beatmapList.size() < 50 && iterator.hasNext()) {
-                    ObjectData /*BeatmapPlayCount*/ beatmapPlayCount = iterator.next();
-                    ObjectData /*Beatmap*/ beatmap = beatmapPlayCount.get("beatmap");
+                    BeatmapPlayCount beatmapPlayCount = iterator.next();
+                    Beatmap beatmap = beatmapPlayCount.beatmap;
 
-                    String playMode = beatmap.get("playMode").asString();
-                    if (!"mania".equals(playMode)) continue;
+                    PlayMode playMode = beatmap.playMode;
+                    if (playMode != PlayMode.MANIA) continue;
 
-                    String status = beatmap.get("rankStatus").asString();
-                    if (!"ranked".equals(status)) continue;
+                    RankStatus status = beatmap.rankStatus;
+                    if (status != RankStatus.RANKED) continue;
 
                     beatmapList.add(beatmap);
                 }
@@ -162,27 +151,25 @@ public class ScoreService extends Service implements MessageHandler {
             }
 
             // 异步批量 post 获取谱面
-            List<ServiceRequest> requestsForBeatmaps = new ArrayList<>();
+            List<Supplier<Optional<List<Beatmap>>>> beatmapGetters = new ArrayList<>();
 
-            for (List<ObjectData /*Beatmap*/> list : beatmapsToPost) {
-                List<ObjectData /*Long*/> ids = list.stream()
-                        .map(o -> o.get("id"))
+            for (List<Beatmap> list : beatmapsToPost) {
+                List<Long> ids = list.stream()
+                        .map(beatmap -> beatmap.id)
                         .toList();
-                requestsForBeatmaps.add(ServiceRequest.builder()
-                        .header("service", "getBeatmaps")
-                        .data("ids", ObjectData.of(ids))
-                        .build());
+                beatmapGetters.add(() -> this.osuApiService.getBeatmaps(ids));
             }
 
-            List<ObjectData /*List<Beatmap>*/> beatmapsExtended = this.osuApiService.call(requestsForBeatmaps);
+            List<Beatmap> beatmapsExtended = MutsumiUtils.getAsync(beatmapGetters).stream()
+                    .flatMap(Optional::stream)
+                    .flatMap(List::stream)
+                    .toList();
 
             // 筛选7K谱面
-            List<ObjectData /*Beatmap*/> beatmaps = new ArrayList<>();
-            for (ObjectData /*List<Beatmap>*/ o : beatmapsExtended) {
-                for (ObjectData /*Beatmap*/ beatmap : ListObjectData.read(o)) {
-                    if (beatmap.getInt("cs") == 7) {
-                        beatmaps.add(beatmap);
-                    }
+            List<Beatmap> beatmaps = new ArrayList<>();
+            for (Beatmap beatmap : beatmapsExtended) {
+                if (beatmap.cs == 7) {
+                    beatmaps.add(beatmap);
                 }
             }
 
@@ -190,32 +177,30 @@ public class ScoreService extends Service implements MessageHandler {
             LOGGER.info("beatmaps size: {}", beatmaps.size());
 
             // 异步批量 post 获取谱面成绩列表
-            List<ServiceRequest> requestsForScores = beatmaps.stream()
-                    .map(beatmap -> ServiceRequest.builder()
-                            .header("service", "getUserBeatmapScores")
-                            .header("user", id)
-                            .header("beatmap", beatmap.getString("id"))
-                            .build())
-                    .toList();
-            List<ObjectData /*List<Score>*/> scores = this.osuApiService.call(requestsForScores);
+            List<Supplier<Optional<List<Score>>>> scoreGetters = new ArrayList<>();
+            for (Beatmap beatmap : beatmaps) {
+                scoreGetters.add(() -> this.osuApiService.getUserBeatmapScores(id, beatmap.id));
+            }
+            List<Optional<List<Score>>> scores = MutsumiUtils.getAsync(scoreGetters);
 
-            List<Pair<ObjectData /*Beatmap*/, ObjectData /*Score*/>>
-                    beatmapsToScores = new ArrayList<>();
+            List<Pair<Beatmap, Score>> beatmapsToScores = new ArrayList<>();
 
             // 由于最好成绩不等于 PP 最高, 这里要比较出 PP 最高的成绩
             // 同时计算 Bonus PP 所需要的成绩总数 N
             int scoresCount = 0;
             for (int i = 0; i < scores.size(); i++) {
-                ObjectData /*Score*/ score = ObjectData.EMPTY;
-                for (ObjectData /*Score*/ scorex : ListObjectData.read(scores.get(i))) {
-                    scoresCount++;
-                    if (score.isEmpty() || score.getDouble("pp") < scorex.getDouble("pp")) {
-                        score = scorex;
-                    }
+                Optional<List<Score>> optional3 = scores.get(i);
+                if (optional3.isEmpty()) {
+                    continue;
                 }
 
-                if (!score.isEmpty()) {
-                    beatmapsToScores.add(new Pair<>(beatmaps.get(i), score));
+                List<Score> scoreList = optional3.get();
+                scoresCount += scoreList.size();
+                Optional<Score> optional4 = scoreList.stream()
+                        .max(Comparator.comparingDouble(score -> score.pp));
+
+                if (optional4.isPresent()) {
+                    beatmapsToScores.add(new Pair<>(beatmaps.get(i), optional4.get()));
                 }
             }
 
@@ -224,15 +209,14 @@ public class ScoreService extends Service implements MessageHandler {
 
             // 按 PP 从高到低排序
             beatmapsToScores.sort(Comparator
-                    .comparingDouble((Pair<ObjectData /*Beatmap*/, ObjectData /*Score*/> value)
-                            -> value.right().getDouble("pp"))
+                    .comparingDouble((Pair<Beatmap, Score> value) -> value.right().pp)
                     .reversed());
 
             // 计算 BP 的总 PP, 加权求和
             double pp = 0.0D, multiplier = 1.0D;
-            for (Pair<ObjectData /*Beatmap*/, ObjectData /*Score*/>
+            for (Pair<Beatmap, Score>
                     beatmapsetsToScore : beatmapsToScores) {
-                pp += multiplier * beatmapsetsToScore.right().getDouble("pp");
+                pp += multiplier * beatmapsetsToScore.right().pp;
                 multiplier *= 0.95D;
             }
 
@@ -249,16 +233,16 @@ public class ScoreService extends Service implements MessageHandler {
 
             int limit = Math.min(30, beatmapsToScores.size());
             for (int i = 0; i < limit; i++) {
-                ObjectData /*Beatmap*/ beatmap = beatmapsToScores.get(i).left();
-                ObjectData /*Beatmapset*/ beatmapset = beatmap.get("beatmapset");
+                Beatmap beatmap = beatmapsToScores.get(i).left();
+                Beatmapset beatmapset = beatmap.beatmapset;
                 builder.append("\n")
-                        .append(beatmapset.getString("artist"))
+                        .append(beatmapset.artist)
                         .append(" - ")
-                        .append(beatmapset.getString("title"))
+                        .append(beatmapset.title)
                         .append(" (")
-                        .append(beatmap.getString("version"))
+                        .append(beatmap.version)
                         .append("): ")
-                        .append(beatmapsToScores.get(i).right().getDouble("pp"))
+                        .append(beatmapsToScores.get(i).right().pp)
                         .append("pp");
             }
 
