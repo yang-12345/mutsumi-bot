4d10ecc0547da3366d6254147e60e9517fa72a36
yang_12345 <1716437252@qq.com>
2026-05-01 02:13:24 +0800

    更改: 服务加载和卸载添加 synchronized

--- app/src/main/java/io/github/rikkakawaii0612/mutsumi/loader/MutsumiServiceLoader.java
+++ app/src/main/java/io/github/rikkakawaii0612/mutsumi/loader/MutsumiServiceLoader.java
@@ -32,169 +32,178 @@ public class MutsumiServiceLoader {
     private final Map<String, JsonNode> configs = new HashMap<>();
     private final Config configImpl = id -> this.configs.computeIfAbsent(id, this::createConfig);
 
+    // 上线程锁, 防止控制台异步同时调用 load() 和 unload()
+    private final Object lock = new Object();
+
     public MutsumiServiceLoader(MutsumiImpl mutsumi) {
         this.mutsumi = mutsumi;
     }
 
     public void load() {
-        if (this.classLoader != null) {
-            LOGGER.warn("Trying to load service loader that has been loaded");
-            this.unload();
-        }
+        synchronized (this.lock) {
+            if (this.classLoader != null) {
+                LOGGER.warn("Trying to load service loader that has been loaded");
+                this.unload();
+            }
 
-        this.loadConfigs();
+            this.loadConfigs();
 
-        // 读取目录的服务 JAR 文件, 并创建 ClassLoader
-        try {
-            File modsDir = new File("services");
-            if (!modsDir.isDirectory()) {
-                Files.createDirectory(modsDir.toPath());
+            // 读取目录的服务 JAR 文件, 并创建 ClassLoader
+            try {
+                File modsDir = new File("services");
+                if (!modsDir.isDirectory()) {
+                    Files.createDirectory(modsDir.toPath());
+                    return;
+                }
+                File[] files = modsDir.listFiles((_, name) -> name.toLowerCase().endsWith(".jar"));
+                if (files == null || files.length == 0) {
+                    LOGGER.info("No services were found");
+                    return;
+                }
+
+                URL[] jarUrls = new URL[files.length];
+                for (int i = 0; i < files.length; i++) {
+                    jarUrls[i] = files[i].toURI().toURL();
+                }
+
+                ClassLoader parent = MutsumiServiceLoader.class.getClassLoader();
+                this.classLoader = new URLClassLoader(jarUrls, parent);
+            } catch (Exception e) {
+                LOGGER.error("Failed to create class loader: ", e);
                 return;
             }
-            File[] files = modsDir.listFiles((_, name) -> name.toLowerCase().endsWith(".jar"));
-            if (files == null || files.length == 0) {
-                LOGGER.info("No services were found");
-                return;
-            }
 
-            URL[] jarUrls = new URL[files.length];
-            for (int i = 0; i < files.length; i++) {
-                jarUrls[i] = files[i].toURI().toURL();
-            }
-
-            ClassLoader parent = MutsumiServiceLoader.class.getClassLoader();
-            this.classLoader = new URLClassLoader(jarUrls, parent);
-        } catch (Exception e) {
-            LOGGER.error("Failed to create class loader: ", e);
-            return;
-        }
-
-        // 读取服务元数据
-        try {
-            for (URL url : this.classLoader.getURLs()) {
-                try (JarFile jarFile = new JarFile(url.getFile())) {
-                    Manifest manifest = jarFile.getManifest();
-                    Attributes attrs = manifest.getMainAttributes();
-                    String id = attrs.getValue("Service-Id");
-                    String version = attrs.getValue("Service-Version");
-                    String author = attrs.getValue("Service-Author");
-                    String dependenciesField = attrs.getValue("Service-Dependencies");
-                    String mainClass = attrs.getValue("Service-Class");
-                    if (id == null) {
-                        LOGGER.warn("Found service that doesn't have an id. Ignored");
-                        continue;
-                    }
-                    if (version == null) {
-                        LOGGER.warn("Service {} doesn't have field 'Service-Version'. Ignored", id);
-                        continue;
-                    }
-                    if (author == null) {
-                        LOGGER.warn("Service {} doesn't have field 'Service-Author'. Ignored", id);
-                        continue;
-                    }
-                    if (mainClass == null) {
-                        LOGGER.warn("Service {} doesn't have field 'Service-Class'. Ignored", id);
-                        continue;
-                    }
-
-                    List<String> dependencies = new ArrayList<>();
-                    if (dependenciesField != null) {
-                        // 依赖用逗号隔开, 两个依赖间的空格会被忽略
-                        String[] arr = dependenciesField.trim().split(",\b*");
-                        for (String str : arr) {
-                            dependencies.add(str.trim());
+            // 读取服务元数据
+            try {
+                for (URL url : this.classLoader.getURLs()) {
+                    try (JarFile jarFile = new JarFile(url.getFile())) {
+                        Manifest manifest = jarFile.getManifest();
+                        Attributes attrs = manifest.getMainAttributes();
+                        String id = attrs.getValue("Service-Id");
+                        String version = attrs.getValue("Service-Version");
+                        String author = attrs.getValue("Service-Author");
+                        String dependenciesField = attrs.getValue("Service-Dependencies");
+                        String mainClass = attrs.getValue("Service-Class");
+                        if (id == null) {
+                            LOGGER.warn("Found service that doesn't have an id. Ignored");
+                            continue;
                         }
+                        if (version == null) {
+                            LOGGER.warn("Service {} doesn't have field 'Service-Version'. Ignored", id);
+                            continue;
+                        }
+                        if (author == null) {
+                            LOGGER.warn("Service {} doesn't have field 'Service-Author'. Ignored", id);
+                            continue;
+                        }
+                        if (mainClass == null) {
+                            LOGGER.warn("Service {} doesn't have field 'Service-Class'. Ignored", id);
+                            continue;
+                        }
+
+                        List<String> dependencies = new ArrayList<>();
+                        if (dependenciesField != null) {
+                            // 依赖用逗号隔开, 两个依赖间的空格会被忽略
+                            String[] arr = dependenciesField.trim().split(",\b*");
+                            for (String str : arr) {
+                                dependencies.add(str.trim());
+                            }
+                        }
+
+                        Class<?> clazz = this.classLoader.loadClass(mainClass);
+                        Service service = (Service) clazz.getConstructor().newInstance();
+                        this.idsToServices.put(id, new ServiceLookup.Wrapper(
+                                service, id, version, author, dependencies
+                        ));
                     }
-
-                    Class<?> clazz = this.classLoader.loadClass(mainClass);
-                    Service service = (Service) clazz.getConstructor().newInstance();
-                    this.idsToServices.put(id, new ServiceLookup.Wrapper(
-                            service, id, version, author, dependencies
-                    ));
                 }
+            } catch (Exception e) {
+                LOGGER.error("Failed to load metadata of services: ", e);
+                this.unload();
+                return;
             }
-        } catch (Exception e) {
-            LOGGER.error("Failed to load metadata of services: ", e);
-            this.unload();
-            return;
-        }
 
-        // 加载服务
-        try {
-            Map<ServiceLookup.Wrapper, List<ServiceLookup.Wrapper>> dependencies = new HashMap<>();
-            for (ServiceLookup.Wrapper wrapper : this.idsToServices.values()) {
-                List<ServiceLookup.Wrapper> list = new ArrayList<>();
-                for (String id : wrapper.dependencies()) {
-                    if (!this.idsToServices.containsKey(id)) {
-                        throw new ClassNotFoundException(
-                                "Service '" + wrapper.id() + "' requires '" + id
-                                        + "' as a dependency, which is missing!");
+            // 加载服务
+            try {
+                Map<ServiceLookup.Wrapper, List<ServiceLookup.Wrapper>> dependencies = new HashMap<>();
+                for (ServiceLookup.Wrapper wrapper : this.idsToServices.values()) {
+                    List<ServiceLookup.Wrapper> list = new ArrayList<>();
+                    for (String id : wrapper.dependencies()) {
+                        if (!this.idsToServices.containsKey(id)) {
+                            throw new ClassNotFoundException(
+                                    "Service '" + wrapper.id() + "' requires '" + id
+                                            + "' as a dependency, which is missing!");
+                        }
+                        list.add(this.idsToServices.get(id));
                     }
-                    list.add(this.idsToServices.get(id));
+                    dependencies.put(wrapper, list);
                 }
-                dependencies.put(wrapper, list);
+
+                ServiceLookup lookup = new ServiceLookup(this.mutsumi, this.idsToServices, this.configImpl);
+
+                // 按依赖关系进行拓扑排序
+                List<ServiceLookup.Wrapper> sorted = MathUtils.topologicalSort(this.idsToServices.values(), dependencies);
+
+                for (ServiceLookup.Wrapper wrapper : sorted) {
+                    wrapper.service().load(wrapper.id(), lookup);
+                }
+
+            } catch (Exception e) {
+                LOGGER.error("Failed to load services: ", e);
+                this.unload();
+                return;
             }
 
-            ServiceLookup lookup = new ServiceLookup(this.mutsumi, this.idsToServices, this.configImpl);
-
-            // 按依赖关系进行拓扑排序
-            List<ServiceLookup.Wrapper> sorted = MathUtils.topologicalSort(this.idsToServices.values(), dependencies);
-
-            for (ServiceLookup.Wrapper wrapper : sorted) {
-                wrapper.service().load(wrapper.id(), lookup);
-            }
-
-        } catch (Exception e) {
-            LOGGER.error("Failed to load services: ", e);
-            this.unload();
-            return;
+            LOGGER.info("Loaded {} service(s)", this.idsToServices.size());
         }
-
-        LOGGER.info("Loaded {} service(s)", this.idsToServices.size());
     }
 
     public void unload() {
-        if (this.classLoader == null) {
-            return;
-        }
-
-        boolean successful = true;
-        for (ServiceLookup.Wrapper wrapper : this.idsToServices.values()) {
-            try {
-                wrapper.service().unload();
-            } catch (Exception e) {
-                LOGGER.error("Failed to unload service {}, which may occur memory leak: ", wrapper.id(), e);
-                successful = false;
+        synchronized (this.lock) {
+            if (this.classLoader == null) {
+                return;
             }
-        }
 
-        if (successful) {
-            LOGGER.info("Unloaded all services");
-        } else {
-            LOGGER.warn("Some services are not unloaded successfully. Be aware!");
-        }
+            this.mutsumi.getBotBus().clearMessageHandlers();
+
+            boolean successful = true;
+            for (ServiceLookup.Wrapper wrapper : this.idsToServices.values()) {
+                try {
+                    wrapper.service().unload();
+                } catch (Exception e) {
+                    LOGGER.error("Failed to unload service {}, which may occur memory leak: ", wrapper.id(), e);
+                    successful = false;
+                }
+            }
+
+            if (successful) {
+                LOGGER.info("Unloaded all services");
+            } else {
+                LOGGER.warn("Some services are not unloaded successfully. Be aware!");
+            }
 
 
-        ReferenceQueue<ClassLoader> queue = new ReferenceQueue<>();
-        WeakReference<ClassLoader> weakReference = new WeakReference<>(this.classLoader, queue);
+            ReferenceQueue<ClassLoader> queue = new ReferenceQueue<>();
+            WeakReference<ClassLoader> weakReference = new WeakReference<>(this.classLoader, queue);
 
-        try {
-            this.classLoader.close();
-        } catch (IOException e) {
-            LOGGER.error("Failed to close service class loader, which may occur memory leak: ", e);
-        } finally {
-            this.classLoader = null;
-        }
+            try {
+                this.classLoader.close();
+            } catch (IOException e) {
+                LOGGER.error("Failed to close service class loader, which may occur memory leak: ", e);
+            } finally {
+                this.classLoader = null;
+            }
 
-        // 检查类加载器是否在一秒内被回收, 报告可能的内存泄漏
-        System.gc();
-        try {
-            Thread.sleep(100L);
-        } catch (InterruptedException e) {
-            LOGGER.error("Thread was interrupted while checking GC of service class loader", e);
-        } finally {
-            if (Objects.equals(queue.poll(), weakReference)) {
-                LOGGER.warn("Service class loader seems to be not garbage-collected, which may occur memory leak!");
+            // 检查类加载器是否在一秒内被回收, 报告可能的内存泄漏
+            System.gc();
+            try {
+                Thread.sleep(100L);
+            } catch (InterruptedException e) {
+                LOGGER.error("Thread was interrupted while checking GC of service class loader", e);
+            } finally {
+                if (Objects.equals(queue.poll(), weakReference)) {
+                    LOGGER.warn("Service class loader seems to be not garbage-collected, which may occur memory leak!");
+                }
             }
         }
     }
