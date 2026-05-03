248aab22da938510348aa41b9da03f2de1a1c71e
yang_12345 <1716437252@qq.com>
2026-05-03 17:43:53 +0800

    修复: 遮罩包含的内容没有抗锯齿

--- osu-image/src/main/java/io/github/rikkakawaii0612/mutsumi/osuImage/core/Mask.java
+++ osu-image/src/main/java/io/github/rikkakawaii0612/mutsumi/osuImage/core/Mask.java
@@ -16,13 +16,41 @@ public class Mask extends Group {
 
     @Override
     public void render(Graphics2D g) {
-        BufferedImage image = new BufferedImage(this.getWidth(), this.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        int w = this.getWidth();
+        int h = this.getHeight();
+
+        BufferedImage mask = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D gMask = mask.createGraphics();
+        gMask.setRenderingHint(RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_QUALITY);
+        gMask.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        gMask.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        this.mask.render(gMask);
+        gMask.dispose();
+
+        BufferedImage image = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2d = image.createGraphics();
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         super.render(g2d);
 
-        g2d.setComposite(AlphaComposite.SrcOut);
-        this.mask.render(g2d);
-        g2d.dispose();
+        int[] maskPixels = mask.getRGB(0, 0, w, h, null, 0, w);
+        int[] resultPixels = image.getRGB(0, 0, w, h, null, 0, w);
+        for (int i = 0; i < resultPixels.length; i++) {
+            int maskAlpha = maskPixels[i] >>> 24;
+            if (maskAlpha == 0) continue;
+
+            int pixel = resultPixels[i];
+            int alpha = (255 - maskAlpha) * (pixel >>> 24) / 255;
+            resultPixels[i] = (pixel & 0x00FFFFFF) | (alpha << 24);
+        }
+        image.setRGB(0, 0, w, h, resultPixels, 0, w);
 
         g.drawImage(image, 0, 0, null);
     }
