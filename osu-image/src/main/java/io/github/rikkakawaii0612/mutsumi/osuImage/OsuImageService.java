478da6ee0cc339d060419d4e3559fe42bc049673
yang_12345 <1716437252@qq.com>
2026-05-03 17:50:14 +0800

    新增: OsuImageService 自带 Torus 字体

--- osu-image/src/main/java/io/github/rikkakawaii0612/mutsumi/osuImage/OsuImageService.java
+++ osu-image/src/main/java/io/github/rikkakawaii0612/mutsumi/osuImage/OsuImageService.java
@@ -2,14 +2,38 @@ package io.github.rikkakawaii0612.mutsumi.osuImage;
 
 import io.github.rikkakawaii0612.mutsumi.api.Service;
 import io.github.rikkakawaii0612.mutsumi.api.ServiceLookup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-// 事实上这个类没啥用, 纯占位符
+import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class OsuImageService implements Service {
+    private static final Logger LOGGER = LoggerFactory.getLogger("OsuImageService");
+
     @Override
     public void load(String id, ServiceLookup lookup) {
+        loadFont("assets/fonts/Torus-Light.otf");
+        loadFont("assets/fonts/Torus-Regular.otf");
+        loadFont("assets/fonts/Torus-SemiBold.otf");
     }
 
     @Override
     public void unload() {
+    }
+
+    private static void loadFont(String path) {
+        try (InputStream fontStream = OsuImageService.class.getClassLoader().getResourceAsStream(path)) {
+            if (fontStream == null) {
+                LOGGER.warn("Font file not found at: {}", path);
+                return;
+            }
+            Font font = Font.createFont(Font.TRUETYPE_FONT, fontStream);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(font);
+        } catch (FontFormatException | IOException e) {
+            LOGGER.warn("Failed to load font file at '{}': ", path, e);
+        }
     }
 }
