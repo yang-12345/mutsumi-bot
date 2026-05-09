9f5befd264ba72051b1caa888e6416a8f73d09d6
yang_12345 <1716437252@qq.com>
2026-05-09 21:03:28 +0800

    更改: 给希腊字母生成添加了频率限制 (10s 一次)

--- osu-image/src/main/java/io/github/rikkakawaii0612/mutsumi/osuImage/OsuImageService.java
+++ osu-image/src/main/java/io/github/rikkakawaii0612/mutsumi/osuImage/OsuImageService.java
@@ -1,5 +1,7 @@
 package io.github.rikkakawaii0612.mutsumi.osuImage;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
 import io.github.rikkakawaii0612.mutsumi.api.Mutsumi;
 import io.github.rikkakawaii0612.mutsumi.api.Service;
 import io.github.rikkakawaii0612.mutsumi.api.ServiceLookup;
@@ -17,6 +19,7 @@ import org.slf4j.LoggerFactory;
 import java.awt.*;
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -28,6 +31,21 @@ public class OsuImageService implements Service {
     private Mutsumi mutsumi;
 
     private final Map<Long, Map<Long, GreekInfo>> listeningInfos = new ConcurrentHashMap<>();
+
+    // omg 我 bot 因为希腊字母生成被封了!!! 故在此写一个限流.
+    // 5 个令牌容量, 10 秒补充一个令牌
+    private final Bucket limit = Bucket.builder()
+            .addLimit(Bandwidth.builder()
+                    .capacity(5L)
+                    .refillGreedy(1L, Duration.ofSeconds(10L))
+                    .build())
+            .build();
+
+    // 达到限制后的回复也得限制频率, 在恢复容量之前不会再提示
+    private boolean repliedLimitReached = false;
+
+    // 因为要写限流 所以把锁加回来了
+    private final Object lock = new Object();
 
     @Override
     public void load(String id, ServiceLookup lookup) {
@@ -127,6 +145,21 @@ public class OsuImageService implements Service {
         } else {
             GreekBackgroundGenerator.Param param = pair.right();
             greekInfo = new GreekInfo(l, pair.left(), param.chromaticAberration(), param.glitch());
+        }
+
+        // 限流这一块
+        // 消耗令牌成功则取消已回复标记, 失败则在回复之后设置已回复标记
+        synchronized (this.lock) {
+            if (this.limit.tryConsume(1L)) {
+                this.repliedLimitReached = false;
+            } else {
+                if (!this.repliedLimitReached) {
+                    bot.sendMessage(groupId, Message.at(senderId)
+                            .append("当前发送希腊字母的请求过多, 请先让 " + this.mutsumi.getName() + " 休息一会儿。"));
+                }
+                this.repliedLimitReached = true;
+                return;
+            }
         }
 
         if (!this.listeningInfos.containsKey(groupId)) {
