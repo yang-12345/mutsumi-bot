e3b3214878732ba26f04e5cc4153f2ba08306c1f
yang_12345 <1716437252@qq.com>
2026-05-16 17:56:28 +0800

    移除: 导入不了的注解, 反正没啥用

--- api/src/main/java/io/github/rikkakawaii0612/mutsumi/api/MutsumiProvider.java
+++ api/src/main/java/io/github/rikkakawaii0612/mutsumi/api/MutsumiProvider.java
@@ -1,7 +1,5 @@
 package io.github.rikkakawaii0612.mutsumi.api;
 
-import org.jetbrains.annotations.ApiStatus;
-
 /**
  * 用于获取 Mutsumi 单例. 在主程序开始运行后, 其应该能返回 Mutsumi 单例.
  * 若调用时不能确保主程序已初始化, 则可能获得 null.
@@ -27,7 +25,6 @@ public class MutsumiProvider {
      *
      * <p>此方法用于设置 {@link #instance} 字段, 只应当在主模块 (app) 调用.
      */
-    @ApiStatus.Internal
     public static void set(Mutsumi mutsumi) {
         if (instance != null) {
             throw new IllegalStateException("Mutsumi is already set");
